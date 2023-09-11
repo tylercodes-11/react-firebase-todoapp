af26daccb7301400a2e1fe56525e611d82bf5832
Ty Reid <trawx11@gmail.com>
2023-09-10 21:53:03 -0400

    added rounded list item to completed todo

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -4,7 +4,7 @@ import { FaRegTrashAlt} from 'react-icons/fa';
 
 const style = {
     li: `flex justify-between bg-slate-200 rounded-md p-4 my-2 capitalize`,
-    liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize`,
+    liComplete: `flex justify-between bg-slate-400 p-4 my-2 rounded-md capitalize`,
     row: `flex`,
     text: `ml-2 cursor-pointer`,
     textComplete: `ml-2 cursor-pointer line-through`,
